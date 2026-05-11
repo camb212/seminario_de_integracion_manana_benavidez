1ac9a56779cba137b2e956ae7a61cb1af51bc35c
IDIC <alumnos@alumnosuio.ute.edu.ec>
2026-05-11 10:57:21 -0500

    5/11/2026

--- modulo2/shopapi/config/urls.py
+++ modulo2/shopapi/config/urls.py
@@ -1,6 +1,6 @@
+# config/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from store.views import health_check
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- modulo2/shopapi/store/views/health.py
+++ modulo2/shopapi/store/views/health.py
@@ -1,3 +1,4 @@
+# store/views/health.py
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
